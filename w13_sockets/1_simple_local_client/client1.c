9e52820dd200bcb1961464b738f703c75941eae9
overegoz <twoon.kim@gmail.com>
2018-11-25 21:37:40 +0900

    .

--- w13_sockets/1_simple_local_client/client1.c
+++ w13_sockets/1_simple_local_client/client1.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
-#define BOOL int
-#define TRUE 1
-#define FALSE 0
 
 int main(void){
+	int sockfd;
+	int len;
+	struct sockaddr_un address;
+	int result;
+	char ch = 'A';
 
-	printf("Hello world\n");
-	printf("Not this time, man\n");
+	// create a socket for the client
+	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+	// name the socket as agreed with the server
+	address.sun_family = AF_UNIX;	
+	strcpy(address.sun_path, "server_socket");
+	len = sizeof(address);
+
+	// connect to the server's socket
+	result = connect(sockfd, (struct sockaddr *) &address, len);
+
+	if( result == -1 ) {
+		perror("oops: client1");
+		exit(1);
+	}
+
+	// read and write via sockfd
+	write(sockfd, &ch, 1);
+	read(sockfd, &ch, 1);
+	printf("char from server = %c\n", ch);
+	close(sockfd);
 
 	return 0;
 }
